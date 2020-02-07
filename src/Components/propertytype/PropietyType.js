62ed04e2c97b73222bc6fb62907d2956fd4c3763
angel10050 <angelangel10050@gmail.com>
2020-02-07 17:16:51 -0500

    category button just style

--- src/Components/propertytype/PropietyType.js
+++ src/Components/propertytype/PropietyType.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 import "./propietyType.css";
-import Bilding from "./../../img/building.svg";
 
-const PropietyType = () => {
+const PropietyType = ({ image, propiety }) => {
   return (
     <div className="propiety_type_container">
       <div className="base">
         <span className="image_container">
-          <img src={Bilding} alt="propiety_type" className="propiety_image" />
+          <img src={image} alt="propiety_type" className="propiety_image" />
         </span>
       </div>
-      <p className="type_propiety_text">xxxxxx</p>
+      <p className="type_propiety_text">{propiety}</p>
     </div>
   );
 };
