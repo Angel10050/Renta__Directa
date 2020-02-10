121238efbdf345dcdd8a652653ca5d4bb3eb79e7
angel10050 <angelangel10050@gmail.com>
2020-02-09 19:03:16 -0500

    barra de busqueda sin funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import MainInput from "./components/mainInput/MainInput";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <MainInput />
+      </div>
+    );
   }
 }
 
