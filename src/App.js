d782d55500be4ae4745f9724fba791ac8332e3fb
angel10050 <angelangel10050@gmail.com>
2020-02-14 15:38:55 -0500

    initial extended

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import ExtendCard from "./components/extendCard/ExtendCard";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <ExtendCard />
+      </div>
+    );
   }
 }
 
